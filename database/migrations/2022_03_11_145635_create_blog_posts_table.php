9c3c40b52df2aacf012d4a3c39dfffb22e7ee823
Francis <fkahindi@gmail.com>
2022-03-14 10:44:18 +0300

    initial commit'

---
+++ database/migrations/2022_03_11_145635_create_blog_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_posts', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');
+            $table->text('slug')->unique();
+            $table->text('body');
+            $table->text('meta_description');
+            $table->text('meta_keywords');
+            $table->integer('user_id');
+            $table->string('image')->nullable();
+            $table->string('image_caption')->nullable();
+            $table->boolean('published')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_posts');
+    }
+};
